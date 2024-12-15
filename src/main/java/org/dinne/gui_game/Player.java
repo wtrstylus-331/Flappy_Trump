1ba219d16b7a85c0508d30576f779099f38980ac
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-14 19:21:09 -0500

    Created player subclass inherit from DynamicShape

---
+++ src/main/java/org/dinne/gui_game/Player.java
@@ -0,0 +1,11 @@
+package org.dinne.gui_game;
+
+import javafx.scene.shape.Rectangle;
+
+public class Player extends DynamicShape {
+
+    public Player(int width, int height) {
+        super(width, height);
+        super.setPosition(100,100);
+    }
+}
