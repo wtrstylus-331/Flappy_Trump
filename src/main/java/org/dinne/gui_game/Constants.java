2302655ff164d919dafa68c0721602a5492b5966
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-14 20:42:03 -0500

    Added variables for images from resource directory

--- src/main/java/org/dinne/gui_game/Constants.java
+++ src/main/java/org/dinne/gui_game/Constants.java
@@ -1,5 +1,12 @@
 package org.dinne.gui_game;
 
+import javafx.scene.image.Image;
+
 public interface Constants {
     double GRAVITY = 1.01; // adds to player's yVelocity
+
+    Image IMG_1 = new Image(Constants.class.getResourceAsStream("image1.png"));
+    Image IMG_2 = new Image(Constants.class.getResourceAsStream("image2.png"));
+    Image IMG_3 = new Image(Constants.class.getResourceAsStream("image3.png"));
+    Image IMG_WALL = new Image(Constants.class.getResourceAsStream("wall.png"));
 }
