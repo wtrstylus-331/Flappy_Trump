9c5f10edc936286fefe4a5a7e0ec16ae540f52b4
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-14 22:29:52 -0500

    Added pipeobject class to test functionality, works well

--- src/main/java/org/dinne/gui_game/Main.java
+++ src/main/java/org/dinne/gui_game/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
@@ -14,6 +15,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main extends Application implements Constants {
@@ -21,47 +23,30 @@ public class Main extends Application implements Constants {
     final int width = 600;
     final int height = 400;
 
-    private double yVelocity = 0.0;
-    private boolean flapped = false;
-
-    private Random r = new Random();
-    Rectangle character = null;
-    Player plr = new Player(40,20);
-    PipeObject pipe = new PipeObject(50);
+    private Random random = new Random();
+    Player plr = new Player(40,40);
+    PipeObject pipeTest = new PipeObject(50);
+    ArrayList<Image> sprites = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
         Scene scene = new Scene(pane, width, height);
+        initializeSprites();
 
-        character = new Rectangle(50, 50);
-        character.setFill(new ImagePattern(IMG_1));
-        character.setX(250);
-        character.setY(100);
-
-        Rectangle one = new Rectangle(50, 50);
-        one.setFill(Color.BLACK);
-        one.setX(50);
-        one.setY(100);
-
-        Rectangle two = new Rectangle(50, 50);
-        two.setFill(Color.BLACK);
-        two.setX(300);
-        two.setY(75);
-
-        pipe.setX(200);
-        pipe.setY(200);
+        pipeTest.setXPos(250);
+        pipeTest.setYPos(0);
+        pipeTest.setXVelocity(2);
+        pipeTest.draw();
 
         plr.setImage(IMG_1);
         plr.setYVelocity(0.1);
         plr.draw();
 
-        pane.getChildren().add(character);
-        pane.getChildren().add(one);
-        pane.getChildren().add(two);
         pane.getChildren().add(plr);
-        pane.getChildren().add(pipe);
+        pane.getChildren().add(pipeTest.getHolder());
 
+        /*
         AnimationTimer movement = new AnimationTimer() {
             @Override
             public void handle(long l) {
@@ -75,27 +60,15 @@ public class Main extends Application implements Constants {
 
                 //character.setFill(Color.rgb(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
             }
-        };
+        }; */
 
         //movement.start();
 
         scene.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             if (event.getButton() == MouseButton.PRIMARY) {
-                if (!flapped) {
-                    flapped = true;
-
-//                    int random = r.nextInt(120,255);
-//                    character.setFill(Color.rgb(random, random, 0));
-
-                    System.out.println("flapped");
-                    yVelocity = -2.5;
-
-                    PauseTransition pause = new PauseTransition(Duration.seconds(0.25));
-                    pause.setOnFinished(e -> {
-                        flapped = false;
-                    });
-                    pause.play();
-                }
+                int choice = random.nextInt(0,3);
+                plr.setImage(sprites.get(choice));
+                plr.setYVelocity(-2.7);
             }
         });
 
@@ -108,13 +81,19 @@ public class Main extends Application implements Constants {
         launch();
     }
 
+    private void initializeSprites() {
+        for (int i = 0; i < 3; i++) {
+            sprites.add(new Image(getClass().getResourceAsStream("image" + (i + 1) + ".png")));
+        }
+    }
+
     private boolean isCollision(double x, double y, Rectangle other) {
         double otherX = other.getX();
         double otherY = other.getY();
         double otherWidth = other.getWidth();
         double otherHeight = other.getHeight();
 
-        return x < otherX + otherWidth && x + character.getWidth() > otherX
-                && y < otherY + otherHeight && y + character.getHeight() > otherY;
+        return x < otherX + otherWidth && x + plr.getWidth() > otherX
+                && y < otherY + otherHeight && y + plr.getHeight() > otherY;
     }
 }
