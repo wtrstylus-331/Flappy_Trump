8bfc59e7c9c0151f846fd7cffc3ee43d8246341c
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-14 22:30:12 -0500

    Updated class to hold a pane to store pipes for easier functionality for the game

--- src/main/java/org/dinne/gui_game/PipeObject.java
+++ src/main/java/org/dinne/gui_game/PipeObject.java
@@ -1,23 +1,75 @@
 package org.dinne.gui_game;
 
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
+import javafx.animation.AnimationTimer;
+import javafx.scene.layout.Pane;
 
 public class PipeObject extends DynamicShape implements Constants {
+    private Pipe top, down;
+    private Pane holder;
+    private AnimationTimer timer;
+    private double xVelocity;
+    private int width;
 
     public PipeObject(int width) {
         super(width);
+        super.setHeight(0);
+        this.width = width;
 
-        this.setImage(IMG_WALL);
+        holder = new Pane();
+        holder.setPrefWidth(width);
+        holder.setPrefHeight(1000);
+
+        top = new Pipe(width);
+        down = new Pipe(width);
+        top.setY(-250);
+        top.setImage(IMG_WALL);
+        down.setY(250);
+        down.setImage(IMG_WALL);
+
+        holder.getChildren().addAll(top, down);
     }
 
-    @Override
-    public void setImage(Image image) {
-        this.setFill(new ImagePattern(image));
+    public void setXPos(double xPos) {
+        holder.setTranslateX(xPos);
+    }
+
+    public void setYPos(double yPos) {
+        holder.setTranslateY(yPos);
+    }
+
+    public double getXPos() {
+        return holder.getTranslateX();
+    }
+
+    public double getYPos() {
+        return holder.getTranslateY();
+    }
+
+    public void setXVelocity(double xVelocity) {
+        this.xVelocity = xVelocity;
+    }
+
+    public double getxVelocity() {
+        return xVelocity;
+    }
+
+    public Pane getHolder() {
+        return holder;
     }
 
     @Override
     public void draw() {
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                holder.setTranslateX(holder.getTranslateX() - xVelocity);
 
+                if (holder.getTranslateX() <= -width) {
+                    holder.setTranslateX(WIDTH);
+                }
+            }
+        };
+
+        timer.start();
     }
 }
