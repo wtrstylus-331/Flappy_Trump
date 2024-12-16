edeaaba74dd604f200f20580a4d41b0e88f89bd9
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-16 18:16:14 -0500

    Simplified start method and added static method to restart back to title page, implemented custom static audioplayer objects

--- src/main/java/org/dinne/gui_game/Main.java
+++ src/main/java/org/dinne/gui_game/Main.java
@@ -3,26 +3,45 @@ package org.dinne.gui_game;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import org.dinne.gui_game.page.TitlePage;
+import org.dinne.gui_game.util.AudioPlayer;
 import org.dinne.gui_game.util.Constants;
 
 import java.util.ArrayList;
 
 public class Main extends Application implements Constants {
     // Variables
-    final int width = 600;
-    final int height = 400;
+    private final static int width = 600;
+    private final static int height = 400;
 
     public static ArrayList<Image> sprites = new ArrayList<>();
     public static Stage stage;
     public static boolean gameRunning = false;
+    public static AudioPlayer bgMusic = new AudioPlayer(new Media(
+            Main.class.getResource("/org/dinne/gui_game/BackgroundMusic.mp3").toExternalForm()
+    ));
+    public static AudioPlayer sfxPlayer = new AudioPlayer(null);
+
+    public static void restartStage() {
+        Pane pane = new Pane();
+        TitlePage scene = new TitlePage(pane, width, height);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @Override
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
         TitlePage scene = new TitlePage(pane, width, height);
         initializeSprites();
+
+        bgMusic.play();
+        bgMusic.setCycle(MediaPlayer.INDEFINITE);
+        bgMusic.setVolume(0.75f);
 
         stage = primaryStage;
         primaryStage.setTitle("Flappy Trump");
