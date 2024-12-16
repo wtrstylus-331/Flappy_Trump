b233d9d028acae2a00a28ca2ef7d3d85f4a2f68b
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-16 09:34:04 -0500

    Add implementation for media library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.dinne.gui_game {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.media;
 
 
     opens org.dinne.gui_game to javafx.fxml;
