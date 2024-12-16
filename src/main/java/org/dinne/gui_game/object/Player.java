fd3de1b3a0305dd35f6e30e63f836cc4cda9e778
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-16 09:35:25 -0500

    Add image and collision methods logic

--- src/main/java/org/dinne/gui_game/object/Player.java
+++ src/main/java/org/dinne/gui_game/object/Player.java
@@ -1,21 +1,23 @@
 package org.dinne.gui_game.object;
 
 import javafx.animation.AnimationTimer;
-import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
+import org.dinne.gui_game.Main;
 import org.dinne.gui_game.util.Constants;
 import org.dinne.gui_game.util.DynamicShape;
+
+import java.util.Random;
 
 public class Player extends DynamicShape implements Constants {
     private double yVelocity;
     private AnimationTimer timer;
-    private Image currentImg;
+    private int currentImg;
 
     public Player(int width, int height) {
         super(width, height);
-        super.setPosition(260,100);
-        currentImg = IMG_1;
-        this.setImage(IMG_1);
+        super.setPosition(280,100);
+        this.currentImg = 1;
+        this.setImage(0);
     }
 
     public void setYVelocity(double yVelocity) {
@@ -23,15 +25,19 @@ public class Player extends DynamicShape implements Constants {
     }
 
     public double getYVelocity() {
-        return yVelocity;
+        return this.yVelocity;
     }
 
-    public Image getCurrentImage() {
-        return currentImg;
+    public int getCurrentImage() {
+        return this.currentImg;
     }
 
-    public void setImage(Image image) {
-        super.setFill(new ImagePattern(image));
+    public void setImage(int index) {
+        super.setFill(new ImagePattern(Main.sprites.get(index)));
+    }
+
+    public int getRolledImage() {
+        return new Random().nextInt(0,5);
     }
 
     @Override
@@ -45,9 +51,14 @@ public class Player extends DynamicShape implements Constants {
                 double ry = getY();
                 setY(ry + yVelocity);
 
-                if (getY() > HEIGHT || getY() < 0) {
-                    System.out.println("game over");
+                if (!Main.gameRunning) {
                     timer.stop();
+                    setY(getY());
+                }
+
+                if (getY() > HEIGHT - 20 || getY() < 0) {
+                    System.out.println("game over");
+                    Main.gameRunning = false;
                 }
             }
         };
