a43109bc75a7b557bf68921aea44578ad8b87c6b
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-16 09:33:42 -0500

    Testing AudioPlayer class

---
+++ src/main/java/org/dinne/gui_game/util/AudioPlayer.java
@@ -0,0 +1,11 @@
+package org.dinne.gui_game.util;
+
+import javafx.scene.media.MediaPlayer;
+
+public class AudioPlayer {
+    private MediaPlayer mediaPlayer = null;
+
+    public AudioPlayer() {
+
+    }
+}
