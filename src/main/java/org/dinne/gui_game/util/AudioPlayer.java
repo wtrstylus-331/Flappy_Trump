a054a1b6323c095acf66df4d59daeb1ab03a4b15
wtrstylus-331 <saisrikara5@gmail.com>
2024-12-16 18:13:39 -0500

    Audioplayer class with methods to play audio for the game

--- src/main/java/org/dinne/gui_game/util/AudioPlayer.java
+++ src/main/java/org/dinne/gui_game/util/AudioPlayer.java
@@ -1,11 +1,54 @@
 package org.dinne.gui_game.util;
 
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class AudioPlayer {
-    private MediaPlayer mediaPlayer = null;
+    private MediaPlayer mediaPlayer;
+    private Media currentMedia;
 
-    public AudioPlayer() {
+    public AudioPlayer(Media input) {
+        this.currentMedia = input;
 
+        if (this.currentMedia != null) {
+            this.mediaPlayer = new MediaPlayer(this.currentMedia);
+        }
+    }
+
+    public void setCycle(int i) {
+        if (isValid()) {
+            this.mediaPlayer.setCycleCount(i);
+        }
+    }
+
+    public void play() {
+        if (this.currentMedia != null) {
+            this.mediaPlayer = new MediaPlayer(this.currentMedia);
+        }
+
+        this.mediaPlayer.play();
+    }
+
+    public void setVolume(double volume) {
+        this.mediaPlayer.setVolume(volume);
+    }
+
+    public void setMedia(Media media) {
+        this.currentMedia = media;
+
+        if (this.currentMedia != null) {
+            this.mediaPlayer = new MediaPlayer(this.currentMedia);
+        }
+    }
+
+    public Media getMedia() {
+        if (isValid()) {
+            return this.currentMedia;
+        }
+        return null;
+    }
+
+    private boolean isValid() {
+        return this.mediaPlayer != null;
     }
 }
